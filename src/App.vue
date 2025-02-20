92e54e8cc5b672bb68df506d950b0c1a43f9fbe3
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-19 23:00:14 -0300

    Setup inicial do projeto Vite

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// Detecta se está rodando no GitHub Pages ou em outro ambiente
+const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.VERCEL === undefined;
+
+export default defineConfig({
+  base: isGitHubPages ? '/artGallery/' : '/',
+  plugins: [vue()],
+});
