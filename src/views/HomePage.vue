46e0b29ff84728e3674ffb04ce15b9c48a436383
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-20 22:29:28 -0300

    Update view HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -25,72 +25,16 @@
 <script>
 import { ref, computed } from 'vue';
 import { IconSearch } from '@tabler/icons-vue'; // Ícone de busca moderno
-import art01 from '../assets/images/art01.jpg';
-import art02 from '../assets/images/art02.jpg';
-import art03 from '../assets/images/art03.jpg';
-import art04 from '../assets/images/art04.jpg';
-import art05 from '../assets/images/art05.jpg';
-import art06 from '../assets/images/art06.jpg';
-import art07 from '../assets/images/art07.jpg';
-import art08 from '../assets/images/art08.jpg';
-import art09 from '../assets/images/art09.jpg';
-import art10 from '../assets/images/art10.jpg';
-import art11 from '../assets/images/art11.jpg';
-import art12 from '../assets/images/art12.jpg';
-import art13 from '../assets/images/art13.jpg';
-import art14 from '../assets/images/art14.jpg';
-import art15 from '../assets/images/art15.jpg';
-import art16 from '../assets/images/art16.jpg';
-import art17 from '../assets/images/art17.jpg';
-import art18 from '../assets/images/art18.jpg';
-import art19 from '../assets/images/art19.jpg';
-import art20 from '../assets/images/art20.jpg';
-import art21 from '../assets/images/art21.jpg';
-import art22 from '../assets/images/art22.jpg';
-import art23 from '../assets/images/art23.jpg';
-import art24 from '../assets/images/art24.jpg';
-import art25 from '../assets/images/art25.jpg';
-import art26 from '../assets/images/art26.jpg';
-import art27 from '../assets/images/art27.jpg';
-import art28 from '../assets/images/art28.jpg';
+import artworks from '../data/artworks.js';
 
 export default {
     components: { IconSearch },
     setup() {
         const searchQuery = ref('');
-        const artworks = ref([
-            { id: 1, title: 'Obra 01', image: art01 },
-            { id: 2, title: 'Obra 02', image: art02 },
-            { id: 3, title: 'Obra 03', image: art04 },
-            { id: 4, title: 'Obra 04', image: art05 },
-            { id: 5, title: 'Obra 05', image: art06 },
-            { id: 6, title: 'Obra 06', image: art07 },
-            { id: 7, title: 'Obra 07', image: art08 },
-            { id: 8, title: 'Obra 08', image: art09 },
-            { id: 9, title: 'Obra 09', image: art10 },
-            { id: 10, title: 'Obra 10', image: art11 },
-            { id: 11, title: 'Obra 11', image: art12 },
-            { id: 12, title: 'Obra 12', image: art14 },
-            { id: 13, title: 'Obra 13', image: art17 },
-            { id: 14, title: 'Obra 14', image: art18 },
-            { id: 15, title: 'Obra 15', image: art19 },
-            { id: 16, title: 'Obra 16', image: art20 },
-            { id: 17, title: 'Obra 17', image: art21 },
-            { id: 18, title: 'Obra 18', image: art22 },
-            { id: 19, title: 'Obra 19', image: art24 },
-            { id: 20, title: 'Obra 20', image: art25 },
-            { id: 21, title: 'Obra 21', image: art26 },
-            { id: 22, title: 'Obra 22', image: art27 },
-            { id: 23, title: 'Obra 23', image: art28 },
-            { id: 24, title: 'Obra 24', image: art16 },
-            { id: 25, title: 'Obra 25', image: art13 },
-            { id: 26, title: 'Obra 26', image: art15 },
-            { id: 27, title: 'Obra 27', image: art03 },
-            { id: 28, title: 'Obra 28', image: art23 },
-        ]);
+
         // Filtro de busca
         const filteredArtworks = computed(() => {
-            return artworks.value.filter(art =>
+            return artworks.filter(art =>
                 art.title.toLowerCase().includes(searchQuery.value.toLowerCase())
             );
         });
